4170cf437b9181438523327b4bf392a942a68be9
9jbjhngp88-rgb <9jbjhngp88@privaterelay.appleid.com>
2026-04-26 15:53:58 +0200

    test

---
+++ src/genWeightMat.jl
@@ -0,0 +1,31 @@
+function genWeightMat(ages, years, clip, zeroCohorts)
+    nAges=length(ages)
+    nYears=length(years)
+    cohorts=(years[1]-ages[end]):(years[end]-ages[1])
+    Wxt=ones(nAges, nYears)
+    
+    if clip>0
+        cl=ones(clip,clip)
+        triu!(cl,1)
+        Wxt[nAges-clip+1:nAges,1:clip]=cl
+        Wxt[1:clip,nYears-clip+1:nYears]=transpose(cl)
+    end
+    
+    for i in zeroCohorts
+        h= i-cohorts[1]+1-nAges
+        if h<=0
+            col=1
+            row=1-h
+        else
+            col=h+1
+            row=1
+        end
+
+        while col<=nYears && row<=nAges
+            Wxt[row,col]=0
+            col+=1
+            row+=1 
+        end
+    end
+    return Wxt
+end
